403284af1bb6a73f464f6e001278abf1afc5c7e8
Juanca0399 <juancaruizs@github.com>
2017-10-05 14:49:08 -0600

    Pila agregada

--- Tarea_Corta2.cpp
+++ Tarea_Corta2.cpp
@@ -42,12 +42,70 @@ void cola::imprimir(){
     cout<<Cola[i]<<"->";
   }
 }
+
+class pila{
+  private:
+  int Tope;
+  int Pila[5];
+public:
+
+    pila(){
+    Tope = -1;
+    for (int i = 0; i<5 ; i++){
+      Pila[i] = 0;
+      }
+    }
+
+    bool pilaVacia(){return Tope < 0;}
+    void push (int v);
+    void pop ();
+    void imprimir ();
+  };
+  void pila::push (int v)
+  {
+    if (Tope < (5-1))
+    {
+      Tope++;
+      Pila[Tope] = v;
+    }
+    else
+      cout << "La pila esta llena";
+  }
+  void pila::pop (){
+    if (!pilaVacia()){
+      Tope--;
+    }
+    else{
+      cout << "La pila esta vacia";
+    }
+  }
+
+  void pila::imprimir()
+  {
+    for (int i = Tope; i >= 0; i--)
+    {
+      cout << Pila[i] << "->";
+    }
+  }
+
 int main(){
   cola cola;
   cola.insertar(1);
   cola.insertar(3);
   cola.insertar(5);
   cola.imprimir();
+
+  cout << endl;
+
+  pila pila;
+
+  pila.push(2);
+  pila.push(3);
+  pila.push(4);
+  pila.push(5);
+  pila.push(10);
+  pila.imprimir();
+
   cin.get();
   return 0;
 }
