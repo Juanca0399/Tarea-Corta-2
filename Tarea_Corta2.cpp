effa174305aca3125377715351862d82565f1adb
Dchengg <31906856+Dchengg@users.noreply.github.com>
2017-10-14 18:18:40 -0600

    Revert "Merge branch 'master' of https://github.com/Juanca0399/Tarea-Corta-2"
    This reverts commit aa453034dab32978a6e893cec8ffc9c2476c4c8a, reversing
    changes made to f5c1a631bf95d4b8278b05a9564646c7c75e1d5b.

--- Tarea_Corta2.cpp
+++ Tarea_Corta2.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <algorithm>
 #include <string>
+
 using namespace std;
 
 class nodo {
@@ -31,26 +32,6 @@ nodo(string v, nodo * signodo)
 
 typedef nodo *pnodo;
 
-class pila{
-public:
-  int Tope;
-  int Pila[5];
-public:
-
-    pila(){
-    Tope = -1;
-    for (int i = 0; i<5 ; i++){
-      Pila[i] = 0;
-      }
-    }
-
-    bool pilaVacia(){return Tope < 0;}
-    void push (string v);
-    void pop ();
-    void imprimir ();
-    void vaciarPila();
-  };
-
 class cola{
   private:
     int frente;
@@ -69,8 +50,7 @@ class cola{
     void eliminar();
     void imprimir();
     void IngresarExpresion(string expresion);
-    void LeerArchivosEInsertarlosEnLaCola();
-    void CompararExpresiones(pila &pilaNumeros, pila &pilaOperadores);
+
 };
 
 void cola::insertar(pnodo Node){
@@ -115,7 +95,25 @@ void cola::IngresarExpresion(string expresion){
   }
 }
 
-  void pila::push (string v)
+class pila{
+  private:
+  int Tope;
+  int Pila[5];
+public:
+
+    pila(){
+    Tope = -1;
+    for (int i = 0; i<5 ; i++){
+      Pila[i] = 0;
+      }
+    }
+
+    bool pilaVacia(){return Tope < 0;}
+    void push (int v);
+    void pop ();
+    void imprimir ();
+  };
+  void pila::push (int v)
   {
     if (Tope < (5-1))
     {
@@ -139,12 +137,6 @@ void cola::IngresarExpresion(string expresion){
     for (int i = Tope; i >= 0; i--)
     {
       cout << Pila[i] << "->";
-    }
-  }
-
-  void pila::vaciarPila(){
-    while (pilaVacia() == false){
-      pop();
     }
   }
 
@@ -193,27 +185,20 @@ void CrearArchivos(){
   insertar = InputExpresion();
   arch5 << insertar << std::endl;
 }
-
-string LeerContenidoDelArchivo(string archivo){
-  //Antes se llamaba LeerArchivos pero para mayor claridad y poder implementar un nuevo método le cambie el nombre
-  string expresion;
+string LeerArchivos(string archivo){
+  string expresion1;
 
   std::ifstream Archivo(archivo);
-	std::string str;
-  while (std::getline(Archivo, str)) {
-    expresion = str;
+	std::string str1;
+  while (std::getline(Archivo, str1)) {
+    expresion1 = str1;
   }
-  return expresion;
+  return expresion1;
 }
 
-void cola::LeerArchivosEInsertarlosEnLaCola(){
-  //Es una funcion invocadora la cual lee todos los archivos e inserta a la cola (el metodo está en esta parte del código para que todas las funciones a las que invoca estén declaradas)
-  int cont = 1;
-  std::string contEnStr;
-  std::string siguienteACont;
-  string archivo = "Arch1.txt";
+int main(){
+  string archivo;
   string expresion;
-<<<<<<< HEAD
   CrearArchivos();
   archivo = "Arch1.txt";
   expresion = LeerArchivos(archivo);
@@ -224,97 +209,17 @@ void cola::LeerArchivosEInsertarlosEnLaCola(){
   cola.IngresarExpresion(expresion);
   cola.imprimir();
 
-=======
-  while (cont <= 5){
-    expresion = LeerContenidoDelArchivo(archivo);
-    IngresarExpresion(expresion);
-    contEnStr = std::to_string(cont);
-    siguienteACont = std::to_string(cont+1);
-    archivo.replace(archivo.find(contEnStr), contEnStr.length(), siguienteACont);
-    cont++;
-  }
-}
->>>>>>> 71f5b0fad46ec5f75596a2d3d1e6011ed1eea21f
 
-int validarPFP(string v) {
-  //Metodo con las prioridades fuera de la pila
-	if (v == "^")
-		return 4;
-	else if (v == "*" || v == "/")
-		return 2;
-	else if (v == "+" || v == "-")
-		return 1;
-  else
-  	return 5;
-}
+  cout << endl;
 
-int validarPDP(string v) {
-  //Metodo con las prioridades dentro de la pila
-	if (v == "^")
-		return 3;
-	else if (v == "*" || v == "/")
-		return 2;
-	else if (v == "+" || v == "-")
-		return 1;
-	else
-		return 0;
-}
+  pila pila;
 
-void cola::CompararExpresiones(pila &pilaNumeros, pila &pilaOperadores){
-  //Este metodo (aun no terminafo) va a comparar, en todas las expresiones, sus distintas prioridades y las va a insertar en las pilas
-  //NOTA: Al inicio del ciclo for se limbian las pilas enconces cuando se quiera insertar cosas a los arboles se tiene que tomar en cuenta eso
-  int PDP, PFP;
-  for(int i = frente;i<fondo+1;i++){
-    pilaNumeros.vaciarPila();
-    pilaOperadores.vaciarPila();
-    pnodo aux = Cola[i];
-    while(aux != NULL){
-      string v = aux->valor;
-      if (isdigit(v[0])){
-        pilaNumeros.push(v);
-      }
-      else{
-        if (pilaOperadores.pilaVacia())
-          pilaOperadores.push(aux->valor);
-        else{
-          if (aux->valor == ")"){
-            //Aqui va el proceso que sucede cuando el string es ")"
-          }
-          else{
-            PDP = validarPDP(pilaOperadores[Tope]);
-            PFP = validarPFP(aux->valor);
-            if (PFP > PDP)
-              pilaOperadores.push(aux->valor);
-            else{
-              //Aqui va el proceso que sucede cuando PDP >= PFP
-            }
-          }
-        }
-      }
-      aux = aux->siguiente;
-    }
-  }
-}
-
-int main(){
-  bool opcionIncorrecta = true;
-  while (opcionIncorrecta){
-    string opcionMenu;
-    cout << "¿Que desea hacer?" << endl << "    1. Crear nuevos archivos y leerlos" << endl << "    2. Leer archivos preexistentes" << endl;
-    cin >> opcionMenu;
-    if (opcionMenu != "1" && opcionMenu != "2"){
-      cout << "---------------------------------------" << endl << "Digite una opcion valida" << endl << "---------------------------------------" << endl << endl;
-    }
-    else if (opcionMenu == "1"){
-      CrearArchivos();
-      opcionIncorrecta = false;
-    }
-    else
-      opcionIncorrecta = false;
-  }
-  cola cola;
-  cola.LeerArchivosEInsertarlosEnLaCola();
-  cola.imprimir();
+  pila.push(2);
+  pila.push(3);
+  pila.push(4);
+  pila.push(5);
+  pila.push(10);
+  pila.imprimir();
 
   cin.get();
   return 0;
